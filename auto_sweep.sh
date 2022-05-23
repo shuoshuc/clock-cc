baad75ba78f2d5d51df48aee758c7571117036d7
Shawn Chen <sschenshawn@gmail.com>
2022-05-23 14:48:47 -0400

    Create an auto_sweep script.

---
+++ auto_sweep.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+nflow="10"
+for i in {1..10}
+do
+    python3 flowgrind.py
+    mv "fg.log" "data/edge-bbr""$nflow""-run""$i"".log"
+    ./fg_parser.sh "data/edge-bbr""$nflow""-run""$i"".log"
+done
