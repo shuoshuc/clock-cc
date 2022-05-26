c5583060189898d5cb729dcf573927f1a6f5d2b0
Shawn Chen <sschenshawn@gmail.com>
2022-05-26 00:07:05 -0400

    Update auto_sweep.sh

--- auto_sweep.sh
+++ auto_sweep.sh
@@ -5,6 +5,6 @@ for i in {1..10}
 do
     python3 flowgrind.py
     mv "fg.log" "data/edge-bbr""$nflow""-run""$i"".log"
-    ./fg_parser.sh "data/edge-bbr""$nflow""-run""$i"".log"
+    python3 fg_parser.py log "data/edge-bbr""$nflow""-run""$i"".log"
     sleep 5
 done
