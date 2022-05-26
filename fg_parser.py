f68c4cf5b7067b87b600a39a773a08f4ae22b9c1
Shawn Chen <sschenshawn@gmail.com>
2022-05-26 01:19:08 -0400

    Add waitThru function to fg_parser.py

--- fg_parser.py
+++ fg_parser.py
@@ -45,13 +45,36 @@ def avgJFI(folder):
                 if match:
                     tputs.append(float(match.group(1)))
         jfi.append([square(sum(tputs)) / (len(tputs) * sum(square(tputs)))])
-    with open(os.path.join(folder, 'jfi.csv'), 'w', encoding='UTF8') as outf:
+    with open(os.path.join(folder, 'jfi.csv'), 'w') as outf:
         writer = csv.writer(outf)
         writer.writerow(['JFI'])
         writer.writerows(jfi)
+
+def waitThru(folder):
+    wait_tputs = []
+    pattern = re.compile(r".*D:.*read delay = (\d+\.\d+).*through = "
+                         r"\d+\.\d+/(\d+\.\d+) \[Mbit/s\].*")
+    logname_pattern = re.compile(r".*run(\d+)\.log")
+    logs = Path(folder).glob('*.log')
+    for log in logs:
+        log_match = logname_pattern.match(str(log))
+        if log_match:
+            run = log_match.group(1)
+        with open(log, 'r') as f:
+            for line in f:
+                match = pattern.match(line)
+                if match:
+                    wait_tputs.append((run, float(match.group(1)),
+                                       float(match.group(2))))
+    with open(os.path.join(folder, 'wait_tput.csv'), 'w') as outf:
+        writer = csv.writer(outf)
+        writer.writerow(['run', 'wait(sec)', 'thru(Mbps)'])
+        writer.writerows(wait_tputs)
 
 if __name__ == "__main__":
     if sys.argv[1] == 'log':
         logParser(sys.argv[2])
     elif sys.argv[1] == 'jfi':
         avgJFI(sys.argv[2])
+    elif sys.argv[1] == 'wait':
+        waitThru(sys.argv[2])
