bda779ba05253421a612b3e7a028ab93fce359db
Shawn Chen <sschenshawn@gmail.com>
2022-05-26 00:04:37 -0400

    Extend fg_parser.py with a JFI computing function.

--- fg_parser.py
+++ fg_parser.py
@@ -3,6 +3,10 @@
 
 import sys
 import csv  
+import re
+import os.path
+from pathlib import Path
+from numpy import square, sum
 
 def logParser(logname):
     entries = []
@@ -29,5 +33,25 @@ def logParser(logname):
         writer.writerow(['flow', 'time', 'thru(Mbps)', 'cwnd', 'rtt(msec)'])
         writer.writerows(entries)
 
+def avgJFI(folder):
+    tputs = []
+    jfi = []
+    pattern = re.compile(r".*D:.*through = \d+\.\d+/(\d+\.\d+) \[Mbit/s\].*")
+    logs = Path(folder).glob('*.log')
+    for log in logs:
+        with open(log, 'r') as f:
+            for line in f:
+                match = pattern.match(line)
+                if match:
+                    tputs.append(float(match.group(1)))
+        jfi.append([square(sum(tputs)) / (len(tputs) * sum(square(tputs)))])
+    with open(os.path.join(folder, 'jfi.csv'), 'w', encoding='UTF8') as outf:
+        writer = csv.writer(outf)
+        writer.writerow(['JFI'])
+        writer.writerows(jfi)
+
 if __name__ == "__main__":
-    logParser(sys.argv[1])
+    if sys.argv[1] == 'log':
+        logParser(sys.argv[2])
+    elif sys.argv[1] == 'jfi':
+        avgJFI(sys.argv[2])
